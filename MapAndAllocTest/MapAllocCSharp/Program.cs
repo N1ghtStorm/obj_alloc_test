d3a256361ca60b623e6a792393506e91cff08405
Kivenkantaja Kivenkantaja <bushuevdv@mail.ru>
2021-04-26 13:10:23 +0300

    initial

---
+++ MapAndAllocTest/MapAllocCSharp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+namespace MapAllocCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var elems_num = 20000000;
+            //var girls = new List<Girl>() { Capacity = elems_num };
+            var girls = new List<Girl>();
+            //var boys = new List<Boy>() { Capacity = elems_num };
+            var boys = new List<Boy>();
+            ////var girls = new Girl[elems_num];
+            //var boys = new Boy[elems_num];
+            //Console.ReadKey();
+            var sw1 = new Stopwatch();
+            var sw2 = new Stopwatch();
+
+            sw1.Start();
+            for (int i = 0; i < elems_num; i++)
+            {
+                girls.Add(new Girl { Age = i });
+            }
+            sw1.Stop();
+
+            sw2.Start();
+            for (int i = 0; i < elems_num; i++)
+            {
+                boys.Add(new Boy { Age = i });
+            }
+            sw2.Stop();
+
+            var girls_ages = new int[elems_num];
+            var boys_ages = new int[elems_num];
+
+            var swg = new Stopwatch();
+            var swb = new Stopwatch();
+
+            swg.Start();
+            for (int i = 0; i < elems_num; i++)
+            {
+                girls_ages[i] = girls[i].Age;
+            }
+            swg.Stop();
+
+            swb.Start();
+            for (int i = 0; i < elems_num; i++)
+            {
+                boys_ages[i] = boys[i].Age;
+            }
+            swb.Stop();
+
+            Console.WriteLine($"Girls Add: {sw1.ElapsedMilliseconds}");
+            Console.WriteLine($"Boys Add: {sw2.ElapsedMilliseconds}");
+            Console.WriteLine($"Girls: {swg.ElapsedMilliseconds}");
+            Console.WriteLine($"Boys: {swb.ElapsedMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+
+    struct Girl
+    {
+        public int Age;
+        public int F1;
+        public int F2;
+        public int F3;
+        public int F4;
+    }
+    class Boy
+    {
+        public int Age;
+        public int F1;
+        public int F2;
+        public int F3;
+        public int F4;
+    }
+}
---
+++ MapAndAllocTest/human.js
@@ -0,0 +1,21 @@
+let elems_num = 20000000;
+
+//let humans = new Array(elems_num);
+// 
+let humans = [];
+
+console.time("alloc");
+for (let i = 0; i < elems_num; i++)
+{
+    humans.push({Age: i, F1: 0, F2: 0, F3: 0, F4: 0});
+}
+console.timeEnd("alloc");
+
+let humans_ages = new Array(10000000).fill(0);
+
+console.time("map");
+for (let i = 0; i < elems_num; i++)
+{
+    humans_ages[i] = humans[i].Age;
+}
+console.timeEnd("map");
